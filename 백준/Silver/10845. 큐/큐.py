b32d4092d5cd51ef807b3fb5c1e66c4c1acc791d
Hongkyu Koh <113838846+koh-hongQ@users.noreply.github.com>
2025-07-05 21:02:18 +0900

    [Silver IV] Title: 큐, Time: 48 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,35 @@
+import sys
+a=int(sys.stdin.readline())
+
+c=[]
+for i in range(a):
+	b=sys.stdin.readline().split()
+	
+	if b[0]=='push':
+		c+=[b[1]]
+	elif b[0]=='pop':
+		if len(c)==0:
+			print(-1)
+		else:
+			print(c[0])
+			del c[0]
+	
+	elif b[0]=='size':
+		print(len(c))
+	
+	elif b[0]=='empty':
+		if len(c)==0:
+			print(1)
+		else:
+			print(0)
+	
+	elif b[0]=='front':
+		if len(c)==0:
+			print(-1)
+		else:
+			print(c[0])
+	elif b[0]=='back':
+		if len(c)==0:
+			print(-1)
+		else:
+			print(c[len(c)-1])
